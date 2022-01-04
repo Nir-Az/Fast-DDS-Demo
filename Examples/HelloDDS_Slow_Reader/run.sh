1097999040bc2211086567d7c0c8888efd6ef206
Nir Azkiel <nir.azkiel@intel.com>
2022-01-04 14:27:14 +0200

    update run script

--- Examples/HelloDDS_Slow_Reader/run.sh
+++ Examples/HelloDDS_Slow_Reader/run.sh
@@ -1,9 +1,9 @@
 echo "lunching publisher"
-./build/DDSHelloWorldExampleZeroCopy publisher &
+./build/DDSHelloWorldExampleZeroCopy publisher sim &
 sleep 2
-echo "lunching subscriber I"
-./build/DDSHelloWorldExampleZeroCopy subscriber &
+echo "lunching slow subscriber"
+./build/DDSHelloWorldExampleZeroCopy subscriber slow use_copy &
 sleep 2
 echo "lunching subscriber 2"
-./build/DDSHelloWorldExampleZeroCopy subscriber
+./build/DDSHelloWorldExampleZeroCopy subscriber use_copy
 
